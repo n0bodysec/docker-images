62ca54156a646b0ca17607b367e1daee3616023c
Nobody <git@n0bodysec.com>
2023-02-22 06:05:26 -0300

    refactor(fosscord): migrate to latest version

--- fosscord/data/scripts/update.sh
+++ fosscord/data/scripts/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$1" = "--clean" ] || [ "$1" = "-c" ]; then git -C $WORK_DIR/src/ clean -Xdf; fi
-git -C $WORK_DIR/src/ checkout staging
+git -C $WORK_DIR/src/ checkout master
 git -C $WORK_DIR/src/ reset --hard HEAD
 git -C $WORK_DIR/src/ pull
 git -C $WORK_DIR/src/ apply $WORK_DIR/../patches/*.patch
