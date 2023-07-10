e90c08840627f3897d0ceb779beca8eb861bf6df
Nobody <17956512+n0bodysec@users.noreply.github.com>
2023-07-10 09:27:12 -0300

    refactor(yukon): new script version
    chore(yukon): update .gitignore
    feat(yukon): add root compose override
    refactor(yukon): run setup as node user
    refactor(yukon): add assets branch
    refactor(yukon): add phpmyadmin service
    refactor(yukon): wip new scripts

---
+++ yukon/setup.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Check if required commands are available
+check_command()
+{
+	local command=$1
+
+	if ! command -v "$command" &> /dev/null; then
+		echo "$command is not installed. Please install $command before continuing."
+		exit 1
+	fi
+}
+
+check_command "git"
+check_command "node"
+check_command "wget"
+check_command "unzip"
+
+########################################################################################################################################
+
+# Setup
+CURRENT_ARCH=$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/;s/i686/386/;s/armv[0-9]l/armel/;s/armv[0-9]hf/armhf/;s/ppc64le/ppc64le/')
+
+# Clone required repositories
+echo "TASK: Cloning required repositories..."
+git clone -b $CLIENT_BRANCH --depth 1 https://github.com/wizguin/yukon.git client
+git clone -b $SERVER_BRANCH --depth 1 https://github.com/wizguin/yukon-server.git server
+git clone -b $ASSETS_BRANCH --depth 1 $ASSETS_REPOSITORY www/assets
+
+# Download required files
+echo "TASK: Downloading required files..."
+wget -O /tmp/dockerize.tar.gz https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-alpine-linux-$CURRENT_ARCH-$DOCKERIZE_VERSION.tar.gz
+wget -O /tmp/res.json https://api.github.com/repos/ruffle-rs/ruffle/releases
+node -p "require('/tmp/res.json').flatMap(x => x.assets).find(y => y.browser_download_url.includes('selfhosted')).browser_download_url;" | xargs -n1 wget -O /tmp/ruffle.zip
+
+# Build client
+echo "TASK: Building client..."
+cd client
+npm ci
+npm run build
+
+# Build server
+echo "TASK: Building server..."
+cd ../server
+npm ci
+npm run build
+
+# Move files
+echo "TASK: Moving required files..."
+cd .. && cp -r client/assets www && cp -r client/dist/* www
+unzip -d www/assets/scripts/lib/ruffle /tmp/ruffle.zip
+mkdir -p .data/ssl && tar -xzvf /tmp/dockerize.tar.gz -C .data/
+
+echo "Done!"
